c1764e2d7002d8e5dd26fb2d32404594abbbe1be
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 18:31:39 +0100

    add 3d logo design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div className="2xl:w-[1240px] 2xl:mx-auto xl:mx-[5%] mx-[1rem]">
       <Navbar />
+      <div className="h-[25rem]">
+        <iframe
+          src="https://my.spline.design/itexclogo-99d672ac558ea04e4981221b778201eb/"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
   );
 }
