9f3583a4559c78f631d16639c86aefe4ee9aab80
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 16:25:45 +0100

    setup shadcn and font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,31 @@
 import type { Metadata } from "next";
 import { Mulish, Poppins, Roboto } from "next/font/google";
 import "@/styles/globals.css";
+import { cn } from "@/lib/utils";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
-const mulish = Mulish({ subsets: ["latin"], weight: ["400", "700"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-poppins",
+});
+
+const mulish = Mulish({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-mulish",
+});
 
 export const metadata: Metadata = {
   title: "ITEXC Group | Persue excellence",
   description:
     "Empowering businesses with cutting-edge software development, design, and system integration",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,7 +34,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${roboto.className} ${poppins.className} ${mulish.className} `}
+        className={cn(
+          "min-h-screen bg-background font-roboto antialiased",
+          roboto.variable,
+          poppins.variable,
+          mulish.variable
+        )}
       >
         {children}
       </body>
