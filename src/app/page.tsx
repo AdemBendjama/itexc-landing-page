9fcef2f45893b4a1bdd56b21387ebd97001ea3bb
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 16:39:59 +0100

    create initial navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Image from "next/image";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="px-[6.25rem]">
+      <div className="h-[4.28rem] flex items-center">
+        <div className="flex gap-[1.25rem]">
+          <Image
+            src="/ITEXC-GROUP-V-WORDMARK.svg"
+            alt="watermark"
+            width={103}
+            height={32}
+          />
+          <ul className="flex items-center gap-[1.25rem] font-poppins text-base">
+            <li>Home</li>
+            <li>Services</li>
+            <li>Our Teams</li>
+            <li>Digito</li>
+            <li>Consulting</li>
+            <li>More</li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
