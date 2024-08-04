fb4fb4c23cc12ef8a01729ccf98f5031469cc83b
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 15:02:02 +0100

    create initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ITEXC Group | Persue excellence",
+  description:
+    "Empowering businesses with cutting-edge software development, design, and system integration",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
