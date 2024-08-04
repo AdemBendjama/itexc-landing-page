407700e7af12a9707dd5869efb0434d36f00c06f
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 18:53:24 +0100

    export 3d logo to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
+import Logo3D from "@/components/ui/logo-3d";
 import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <div className="xl:w-[1240px] xl:mx-auto lg:mx-[5%] mx-[1rem]">
       <Navbar />
-      <div className="h-[25rem]">
-        <iframe
-          src="https://my.spline.design/itexclogo-99d672ac558ea04e4981221b778201eb/"
-          width="100%"
-          height="100%"
-        />
-      </div>
+      <Logo3D />
     </div>
   );
 }
---
+++ src/components/ui/logo-3d.tsx
@@ -0,0 +1,13 @@
+function Logo3D() {
+  return (
+    <div className="h-[25rem]">
+      <iframe
+        src="https://my.spline.design/itexclogo-99d672ac558ea04e4981221b778201eb/"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
+}
+
+export default Logo3D;
