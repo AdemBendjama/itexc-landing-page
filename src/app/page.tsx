0a764c09b5c56d41ad6cafdf1e561ae0de9e9d2d
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-04 17:56:58 +0100

    add padding to page for mobile and tablets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
-    <div className="2xl:w-[1240px] 2xl:mx-auto xl:px-[5%]">
+    <div className="2xl:w-[1240px] 2xl:mx-auto xl:px-[5%] px-[1rem]">
       <Navbar />
     </div>
   );
