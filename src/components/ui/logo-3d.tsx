04ad12080003ac5d491115daba4326239d70cba0
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-07 09:37:58 +0100

    display static image in mobile devices instead of 3d design

--- src/components/ui/logo-3d.tsx
+++ src/components/ui/logo-3d.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+
 function Logo3D() {
   return (
-    <div className="h-[400px] w-screen mt-[4.375rem] overflow-hidden">
-      <div className="h-[500px] w-screen relative">
+    <div className="sm:h-[400px] h-[300px] w-screen mt-[4.375rem] overflow-hidden">
+      <div className="lg:h-[500px] h-full w-screen relative">
         <iframe
           src="https://my.spline.design/itexclogo-99d672ac558ea04e4981221b778201eb/"
           width="100%"
           height="100%"
-          className="-top-[100px] absolute"
+          className="-top-[100px] absolute lg:block hidden"
+        />
+        <Image
+          src="/itexc-design.jpg"
+          alt="itexc logo 3d design"
+          fill={true}
+          className="object-contain lg:hidden"
         />
       </div>
     </div>
