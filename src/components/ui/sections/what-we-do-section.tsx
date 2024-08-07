da087cc9b634347e8cf1aaecee2047b5e2e34337
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-07 13:50:48 +0100

    make section mobile responsive

--- src/components/ui/sections/what-we-do-section.tsx
+++ src/components/ui/sections/what-we-do-section.tsx
@@ -15,6 +15,15 @@ var settings = {
   slidesToScroll: 1,
   nextArrow: <ChevronRight size={28} color="#ffffff" />,
   prevArrow: <ChevronLeft size={28} color="#ffffff" />,
+  responsive: [
+    {
+      breakpoint: 641,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 function WhatWeDoSection() {
@@ -22,22 +31,22 @@ function WhatWeDoSection() {
     <div className="bg-submit-second text-submit-foreground font-poppins">
       <div className="h-[500px] relative 2xl:w-[1300px] 2xl:mx-auto xl:mx-[4.375rem] lg:mx-[5%] mx-[1rem]">
         <div className="absolute top-0">
-          <div className="lg:w-[9.375rem] lg:h-[13.125rem] sm:w-[8.125rem] sm:h-[11.25rem] w-[6.25rem] h-[8.75rem] relative">
+          <div className="lg:w-[9.375rem] lg:h-[13.125rem] sm:w-[8.125rem] sm:h-[11.25rem] w-[3rem] h-[4rem] relative">
             <Image src="/group-mark.svg" alt="group mark img" fill={true} />
           </div>
         </div>
         <div className="h-full flex items-center justify-center">
           <div className="z-10 w-[43rem] flex flex-col items-center justify-center gap-[0.5rem] text-center">
-            <span className="font-semibold text-[3rem] leading-[150%]">
+            <span className="font-semibold sm:text-[3rem] text-[2rem] leading-[120%]">
               EXPLORE OUR{" "}
               <span className="bg-gradient-to-r from-[#3CCCAE] via-[#F47B2E] to-[#FE0F9E] bg-clip-text text-transparent">
                 GROUP
               </span>
             </span>
-            <span className="font-normal text-[1.125rem] leading-[150%]">
+            <span className="font-normal sm:text-lg text-base leading-[150%]">
               Specialized Divisions for Targeted Excellence
             </span>
-            <div className="w-[38rem]">
+            <div className="sm:w-[38rem] w-[14rem]">
               <Slider {...settings}>
                 <div>
                   <div className="flex items-center justify-center w-[7rem] h-[7rem]">
@@ -89,7 +98,7 @@ function WhatWeDoSection() {
                 </div>
               </Slider>
             </div>
-            <span className="font-normal text-[1.125rem] leading-[150%]">
+            <span className="font-normal sm:text-lg text-base leading-[150%]">
               We offer diverse expertise through dedicated teams, each focused
               on a distinct field to precisely meet our clients varied needs and
               deliver exceptional results.
