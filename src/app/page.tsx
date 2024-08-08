d356010412567716f382f4cfd09e1f0daa9158e0
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-08-08 11:47:21 +0100

    test suspense on 3d design laod time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ import Footer from "@/components/ui/footer";
 import Logo3D from "@/components/ui/logo-3d";
 import Navbar from "@/components/ui/navbar";
 import Sections from "@/components/ui/sections/sections";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Navbar />
-      <Logo3D />
+      <Suspense
+        fallback={
+          <div className="sm:h-[500px] h-[300px] w-screen mt-[4.375rem]" />
+        }
+      >
+        <Logo3D />
+      </Suspense>
       <Sections />
       <Footer />
     </div>
--- src/components/ui/logo-3d.tsx
+++ src/components/ui/logo-3d.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 function Logo3D() {
   return (
     <div className="sm:h-[500px] h-[300px] w-screen mt-[4.375rem]">
-      <div className="h-full w-screen relative">
+      <div className="h-full w-screen relative lg:block hidden">
         <iframe
           src="https://my.spline.design/itexclogo-99d672ac558ea04e4981221b778201eb/"
           width="100%"
           height="100%"
-          className="lg:block hidden"
         />
+      </div>
+      <div className="h-full w-screen relative lg:hidden">
         <Image
           src="/itexc-design.jpg"
           alt="itexc logo 3d design"
           fill={true}
-          className="object-contain lg:hidden"
+          className="object-contain"
         />
       </div>
     </div>
